89ec40df8b362ac0ddcf6cb8e750a8a92ab64f3e
qrkoh <kohqinruo@gmail.com>
2024-11-07 14:23:23 +0800

    added calibration options
    documentation for calibration values can be found in the apds datasheet and/or the .h file. you may wish to add more functions from the library but i've added the ones i think are useful.

--- SensorController.ino
+++ SensorController.ino
@@ -1,11 +1,14 @@
 #include <Wire.h>
-#include <SparkFun_APDS9960.h>
+#include <SparkFun_APDS9960_mod.h>
 
 SparkFun_APDS9960 apds = SparkFun_APDS9960();
-int pw_input[4] = {0,0,0,0};
+#define APDS9960_INT 23
+int isr_flag = 0;
+int pw_input[4] = { 0, 0, 0, 0 };
 int pw_pos = 0;
 bool input = 0;
 bool begin = 1;
+
 /* Direction definitions */
 //   0  DIR_NONE,
 //   1  DIR_LEFT,
@@ -15,25 +18,34 @@ bool begin = 1;
 //   5  DIR_NEAR,
 //   6  DIR_FAR,
 //   7  DIR_ALL
-// };
 
 void setup() {
+
+  // pinMode(APDS9960_INT, INPUT);
   // Initialize Serial port
   Serial.begin(9600);
-  Serial.println();
-  Serial.println(F("--------------------------------"));
-  Serial.println(F("SparkFun APDS-9960 - GestureTest"));
-  Serial.println(F("--------------------------------"));
+
+  // attachInterrupt(APDS9960_INT, interruptRoutine, FALLING);
 
   // Initialize APDS-9960 (configure I2C and initial values)
   if (apds.init()) {
     Serial.println(F("APDS-9960 initialization complete"));
   } else {
     Serial.println(F("Something went wrong during APDS-9960 init!"));
+    //times to retry init?
   }
 
+
   // Start running the APDS-9960 gesture sensor engine
-  if (apds.enableGestureSensor(true)) {
+  if (apds.enableGestureSensor(false) //true to enable interrupt
+      /* calibration values */
+      // && apds.setGestureEnterThresh(40)
+      // && apds.setGestureExitThresh(20)
+      // && apds.setGestureGain(GGAIN_4X)
+      // && apds.setGestureLEDDrive(LED_DRIVE_100MA) //max
+      // && apds.setGestureWaitTime(GWTIME_8_4MS)
+      // && apds.wireWriteDataByte(APDS9960_GPULSE, 0xD4)  // 32us, 20 pulses 0b11,010100
+  ) {
     Serial.println(F("Gesture sensor is now running"));
   } else {
     Serial.println(F("Something went wrong during gesture sensor init!"));
@@ -45,10 +57,18 @@ void loop() {
     Serial.println("Input password:");
     begin = 0;
   }
+  // if (isr_flag == 1) {
+  // detachInterrupt(APDS9960_INT);
   handleGesture();
+  // isr_flag = 0;
+  // attachInterrupt(APDS9960_INT, interruptRoutine, FALLING);
+  // }
   if (!input and pw_pos == 4) {
     sendPW();
   }
+}
+void interruptRoutine() {
+  isr_flag = 1;
 }
 
 void handleGesture() {
@@ -74,7 +94,7 @@ void handleGesture() {
         Serial.println("6 FAR");
         break;
       default:
-        Serial.println("NOT DETECTED");
+        //Serial.println("NOT DETECTED");
         return;
     }
     pw_input[pw_pos++] = gest;
@@ -87,7 +107,7 @@ void handleGesture() {
 void sendPW() {
   //fill in with packet sending stuff
   Serial.print("Your input: ");
-  for (int i = 0; i < pw_pos; i++){
+  for (int i = 0; i < pw_pos; i++) {
     Serial.print(pw_input[i]);
   }
   Serial.println();
@@ -99,6 +119,6 @@ void sendPW() {
 void handleReply(bool pwcorrect) {
   //polling/interrupt to handle reply from backend
   //fill in with packet parsing stuff
-  if (pwcorrect) Serial.println("Unlocking"); //print statements could be replaced with led feedback
+  if (pwcorrect) Serial.println("Unlocking");  //print statements could be replaced with led feedback
   else Serial.println("Try again!");
 }
